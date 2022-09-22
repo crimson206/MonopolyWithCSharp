78e66c8a28833099e80aaa112d3f44e968cf941a
sisung kim <sisung.kim1@gmail.com>
2022-09-22 18:45:16 +0200

    test : add a test for RegisterPlayer()

--- Tests/BoardTesting/BoardTesting.cs
+++ Tests/BoardTesting/BoardTesting.cs
@@ -1,4 +1,3 @@
-
 namespace BoardTests
 {
     [TestClass]
@@ -11,9 +10,23 @@ namespace BoardTests
             Board board = new Board();
             Dictionary<string, int> emptyDictionary = board.PlayerPositions;
 
-            //assert//
+            //Assert//
             int expectedSize = 0;
             Assert.AreEqual(expectedSize, emptyDictionary.Count());
+        }
+        [TestMethod]
+        public void RegisterPlayer_Adds_Player_Position_Element_With_Position_Zero()
+        {
+            //Arrange//
+            Board board = new Board();
+            Player player = new Player("Player");
+
+            //Act//
+            board.RegisterPlayer(player);
+
+            //Assert//
+            int expectedPlayerPosition = 0;
+            Assert.AreEqual(expectedPlayerPosition, board.PlayerPositions["Player"]);
         }
     }
 }
