5ec1d288da0dcc738ead90b2b2a146e90d3659a5
sisung kim <sisung.kim1@gmail.com>
2022-10-03 00:00:22 +0200

    test_with_player

--- src/UI/Program.cs
+++ src/UI/Program.cs
@@ -5,7 +5,13 @@
         Console.Clear();
         MapDrawer mapDrawer = new MapDrawer();
         Console.WindowHeight = 100;
-        mapDrawer.Test(mapWidth:6, mapHeight:5, tileWidth:10, tileHeight:3);
+        mapDrawer.Test(mapWidth:11, mapHeight:11, tileWidth:13, tileHeight:3);
+
+        int[] position = mapDrawer.CreateTileEdgeCollection(mapWidth:11, mapHeight:11, tileWidth:13, tileHeight:3)[16];
+
+        Console.SetCursorPosition(position[0]+1, position[1]+2);
+
+        Console.Write("P1 P2 P3 P4");
 
         Console.ReadKey();
     }
