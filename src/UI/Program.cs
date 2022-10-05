b604adf18c7a2664c59eaf0ac96e46a2e362d3b1
sisung kim <sisung.kim1@gmail.com>
2022-10-06 00:14:32 +0200

    threadtest

--- src/UI/Program.cs
+++ src/UI/Program.cs
@@ -1,13 +1,65 @@
-﻿internal class Program
+﻿using System.Threading;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.Clear();
+        int OrigCol = Console.CursorLeft;
+        int OrigRow = Console.CursorTop;
+        Console.WindowHeight = 200;
+
+        TimeEventer timeEventer = new TimeEventer(OrigCol, OrigRow);
+
+        Thread t = new Thread(ThreadProc);
+        t.Start();
+        AutoResetEvent autoEvent = new AutoResetEvent(false);
         MapDrawer mapDrawer = new MapDrawer();
-        Console.WindowHeight = 100;
-        mapDrawer.Test(mapWidth:4, mapHeight:3, tileWidth:10, tileHeight:3);
+        Timer timer = new Timer(timeEventer.TimeEvent, autoEvent, 1000, 2000);
+        
+        autoEvent.WaitOne();
 
+        timer.Dispose();
 
-        Console.ReadKey();
+        void ThreadProc()
+        {
+            while(Console.ReadKey().Key != ConsoleKey.B)
+            {
+                while(Console.ReadKey().Key != ConsoleKey.A)
+                {
+                    Console.ReadKey();
+                }
+                Console.Write("IamFreeEEE");
+            }
+        }
+
     }
+}
+class TimeEventer
+{
+    public MapDrawer mapDrawer = new MapDrawer();
+    public int origCol;
+    public int origRow;
+
+    public TimeEventer(int origCol, int origRow)
+    {
+        origCol = origCol;
+        origRow = origRow;
+    }
+
+    int index = 0 ;
+    public void TimeEvent(Object stateInfo)
+    {
+        AutoResetEvent autoEvent = (AutoResetEvent)stateInfo;
+        Console.WindowHeight = 200;
+        mapDrawer.OrigCol = origCol + (index%10)*4;
+        mapDrawer.OrigRow = origRow + (index%10)*4;
+        mapDrawer.DrawMap(5, 5, 10, 3);
+        Console.WriteLine(index);
+        index++;
+        if (index == 100)
+        {
+            autoEvent.Set();
+        }
+    }
+
 }
