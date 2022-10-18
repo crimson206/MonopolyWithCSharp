e3506d5c480435d62168d761b55d8da534e9ea06
sisung kim <sisung.kim1@gmail.com>
2022-10-18 22:12:04 +0200

    refactor_remove_unnecessary_file_and_clear_linter

--- src/UI/Program.cs
+++ src/UI/Program.cs
@@ -1,8 +1,12 @@
-﻿internal class Program
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
     }
-
 }
