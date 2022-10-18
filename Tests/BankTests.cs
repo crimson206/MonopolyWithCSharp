d160eaf4ad8aa0b13374a844d0c9e2984ff19dfd
sisung kim <sisung.kim1@gmail.com>
2022-10-18 22:28:34 +0200

    feature_add_BankHandler_cls

---
+++ Tests/BankTests.cs
@@ -0,0 +1,101 @@
+
+namespace Tests
+{
+    [TestClass]
+    public class BankTests
+    {
+        [TestMethod]
+        public void Initialize_Basic_Constructor_BankHandler_With_Balances_With_Initial_Value_1500()
+        {
+            BankHandler bank = new BankHandler();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Assert.AreEqual(bank.Balances[i], 1500);
+            }
+        }
+        public void Initialize_Basic_Constructor_BankHandler_With_Balances_Whose_Size_Is_Four()
+        {
+            BankHandler bank = new BankHandler();
+
+            int sizeOfBalances = bank.Balances.Count();
+            int expectedSize = 4;
+            Assert.AreEqual(sizeOfBalances, expectedSize);
+        }
+        public void Is_Balances_Not_Changed_By_Modificating_Its_Copy()
+        {
+            BankHandler bank = new BankHandler();
+            List<int> copiedBlances = bank.Balances;
+            int previousBalance = bank.Balances[0];
+
+            copiedBlances[0] += 100;
+
+            int balanceAfterAction = bank.Balances[0];
+            Assert.AreEqual(previousBalance, balanceAfterAction);
+        }
+        public void Increase_Balance_At_Index_By_Amount()
+        {
+            BankHandler bank = new BankHandler();
+            int index = 2;
+            int previousBalance = bank.Balances[index];
+            int amount = 100;
+
+            bank.IncreaseBalance(index, amount);
+
+            int increasedAmount = bank.Balances[index] - previousBalance;
+            int expectedIncreasedAmount = 100;
+            Assert.AreEqual(increasedAmount, expectedIncreasedAmount);
+        }
+        public void Decrease_Balance_At_Index_By_Amount()
+        {
+            BankHandler bank = new BankHandler();
+            int index = 2;
+            int previousBalance = bank.Balances[index];
+            int amount = 100;
+
+            bank.DecreaseBalance(index, amount);
+
+            int decreasedAmount =  previousBalance - bank.Balances[index];
+            int expectedDecreasedAmount = 100;
+            Assert.AreEqual(decreasedAmount, expectedDecreasedAmount);
+        }
+        public void Transfer_Money_Fomm_Index_To_Another_By_Amount()
+        {
+            BankHandler bank = new BankHandler();
+            int fromIndex = 0;
+            int toIndex = 1;
+            int previousBalanceOfFromIndex = bank.Balances[fromIndex];
+            int previousBalanceOfToIndex = bank.Balances[toIndex];
+            int amount = 200;
+
+            bank.TransferBalanceFromTo(fromIndex, toIndex, amount);
+
+            int decreasedAmountOfFromIndexBalance = previousBalanceOfFromIndex - bank.Balances[fromIndex];
+            int increasedAmountOfToIndexBalance = bank.Balances[fromIndex] - previousBalanceOfToIndex;
+            int expectedDecreasedAmount = 200;
+            int expectedIncreasedAmount = 200;
+            Assert.AreEqual(decreasedAmountOfFromIndexBalance, expectedDecreasedAmount);
+            Assert.AreEqual(increasedAmountOfToIndexBalance, expectedIncreasedAmount);
+        }
+        public void Is_Negative_Integer_Not_Accepted_By_BankHandler_Methods()
+        {
+            BankHandler bank = new BankHandler();
+            int index1 = 0;
+            int index2 = 1;
+            int negativeInteger = -100;
+
+            Assert.ThrowsException<Exception>(() => bank.DecreaseBalance(index1, negativeInteger));
+            Assert.ThrowsException<Exception>(() => bank.IncreaseBalance(index1, negativeInteger));
+            Assert.ThrowsException<Exception>(() => bank.TransferBalanceFromTo(index1, index2, negativeInteger));            
+        }
+        public void Can_Not_Transfer_Money_To_Self()
+        {
+            BankHandler bank = new BankHandler();
+            int index1 = 1;
+            int someAmount = 100;
+
+            Assert.ThrowsException<Exception>(() => bank.TransferBalanceFromTo(index1, index1, someAmount));            
+        }
+
+    }
+}
---
+++ src/ClassLibrary/Class/BankHandler.cs
@@ -0,0 +1,82 @@
+//-----------------------------------------------------------------------
+// <copyright file="BankHandler.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+/// <summary>
+/// This class stores players' game money and can increase of decrease the money
+/// </summary>
+public class BankHandler
+{
+    private int initialBalance = 1500;
+    private List<int> balances;
+
+    /// <summary>
+    /// This initializes a Bankhandler object with Balances whose size is 4. The initial balance is 1500.
+    /// </summary>
+    public BankHandler()
+    {
+        this.balances = new List<int>();
+        for (int i = 0; i < 4; i++)
+        {
+            this.balances.Add(this.initialBalance);
+        }
+    }
+
+    /// <summary>
+    /// This is a readonly list of players' game money.
+    /// </summary>
+    public List<int> Balances { get => new List<int>(this.balances); private set => this.balances = value; }
+
+    /// <summary>
+    /// It increases one value of balances at the index playerNumber by the amount
+    /// </summary>
+    /// <param name="playerNumber">An integer between 0 and 3</param>
+    /// <param name="amount">A positive integer</param>
+    public void IncreaseBalance(int playerNumber, int amount)
+    {
+        if (amount < 0)
+        {
+            throw new Exception();
+        }
+        else
+        {
+            this.balances[playerNumber] += amount;
+        }
+    }
+
+    /// <summary>
+    /// It decreases one value of balances at the index playerNumber by the amount
+    /// </summary>
+    /// <param name="playerNumber">An integer between 0 and 3</param>
+    /// <param name="amount">A positive integer</param>
+    public void DecreaseBalance(int playerNumber, int amount)
+    {
+        if (amount < 0)
+        {
+            throw new Exception();
+        }
+        else
+        {
+            this.balances[playerNumber] -= amount;
+        }
+    }
+
+    /// <summary>
+    /// It decreases a value of balances at the index fromPlayerNumber
+    /// and increases at the index of toPlayerNumber by the amount
+    /// </summary>
+    /// <param name="fromPlayerNumber">An integer between 0 and 3</param>
+    /// <param name="toPlayerNumber">An integer between 0 and 3</param>
+    /// <param name="amount">>A positive integer</param>
+    public void TransferBalanceFromTo(int fromPlayerNumber, int toPlayerNumber, int amount)
+    {
+        if (amount < 0 || fromPlayerNumber == toPlayerNumber)
+        {
+            throw new Exception();
+        }
+
+        this.DecreaseBalance(fromPlayerNumber, amount);
+        this.IncreaseBalance(toPlayerNumber, amount);
+    }
+}
