c100aaea2816dc0e06c0652a7c7f02fc1deebe49
crimson206 <110409356+crimson206@users.noreply.github.com>
2023-02-01 04:25:01 +0100

    Feature : added Auto-Simulation (#52)
    * Feature : added SincRun method to Game
    
    * Feature : added the auto-simulation mode

--- src/UI/Program.cs
+++ src/UI/Program.cs
@@ -3,11 +3,12 @@
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 //-----------------------------------------------------------------------
+
 internal class Program
 {
     private static void Main(string[] args)
     {
-        do
+        while(true)
         {
             Console.WriteLine("Do you want the statistics mode? Press Y, otherwise N");
             var readKey = Console.ReadKey();
@@ -15,7 +16,7 @@ internal class Program
             if(readKey.KeyChar == 'y'
             || readKey.KeyChar == 'Y')
             {
-                Console.WriteLine("/nStatistics is working. Please wait a minite");
+                Console.WriteLine("/nStatistics is working. Please wait a minute");
                 
                 for (int i = 0; i < 100; i++)
                 {
@@ -30,45 +31,87 @@ internal class Program
                 break;
             }
 
-        } while (true);
+        }
 
-        bool isBoardSmall = true;
+        while(true)
+        {
+            Console.WriteLine("AutoMode = A, Manual = B");
+            var readKey = Console.ReadKey();
+            if(readKey.KeyChar == 'A'
+            || readKey.KeyChar == 'a')
+            {
+                Console.WriteLine("Autometic Simulator is on. Wait a minute");
+                Modes.RunAutoSimulator();
+                break;
+            }
+            else if(readKey.KeyChar == 'B'
+            || readKey.KeyChar == 'b')
+            {
+                Modes.RunManualSimulator();
+                break;
+            }
+        }
+    }
+}
 
-        Game game = new Game(isBoardSmall);
-        Visualizer visualizer = new Visualizer(game.Data, isBoardSmall);
+public static class Modes
+{
+    public static void RunAutoSimulator()
+    {
+        Game game = new Game(isBoardSmall:true);
+        Visualizer visualizer = new Visualizer(game.Data, isBoardSmall:true);
+        visualizer.SetFixedMessage2(string.Format("Auto-Simulator is on. The time interval is 10s"));
+        System.Timers.Timer timer = new System.Timers.Timer(10000);
 
-        ConsoleInteractor prompter = new ConsoleInteractor(visualizer);
-        string oldRecommendedString = string.Empty;
+        timer.Elapsed += (sender, e) => Run5Times(game, visualizer);
+        timer.Start();
+
+        Console.ReadKey();
+    }
+
+    private static void Run5Times(Game game, Visualizer visualizer)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            game.SincRun();
+            visualizer.UpdateLogging();
+        }
+        visualizer.Visualize();
+    }
+
+    public static void RunManualSimulator()
+    {
+
+        Game game = new Game(isBoardSmall:true);
+        Visualizer visualizer = new Visualizer(game.Data, isBoardSmall:true);
+
         int termBetweenVisualizazions = 1;
         int termIndex = 0;
 
         while (true)
         {
-            if (oldRecommendedString != game.Data.EventFlow.RecommendedString)
+
+            termIndex++;
+            visualizer.UpdateLogging();
+            visualizer.SetFixedMessage2(string.Format("Press 1~9 to adjust the progress speed : {0}", termBetweenVisualizazions));
+
+            if (termIndex % termBetweenVisualizazions == 0)
             {
-                termIndex++;
-                visualizer.UpdateLogging();
-                visualizer.SetFixedMessage2(string.Format("Press 1~9 to adjust the progress speed : {0}", termBetweenVisualizazions));
-                oldRecommendedString = game.Data.EventFlow.RecommendedString;
 
-                if (termIndex % termBetweenVisualizazions == 0)
+                visualizer.Visualize();
+                var readKey = Console.ReadKey();
+
+                if (char.IsDigit(readKey.KeyChar))
                 {
-
-                    visualizer.Visualize();
-                    var readKey = Console.ReadKey();
-
-                    if (char.IsDigit(readKey.KeyChar))
+                    int newTerm = int.Parse(readKey.KeyChar.ToString()!);
+                    if (newTerm != 0)
                     {
-                        int newTerm = int.Parse(readKey.KeyChar.ToString()!);
-                        if (newTerm != 0)
-                        {
-                            termBetweenVisualizazions = newTerm;
-                        }
+                        termBetweenVisualizazions = newTerm;
                     }
                 }
             }
 
-            game.Run();
+            game.SincRun();
         }
     }
 }
